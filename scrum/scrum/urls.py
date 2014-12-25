7d2f97d678db055f4ae62384270de9535e2e63a5
Justyna Ilczuk <att@atte.ro>
2014-12-25 03:22:12 -0800

    Initial commit

---
+++ scrum/scrum/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = patterns('',
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
+)
+
